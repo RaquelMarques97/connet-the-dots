71d16b18ddfe5b02c4697588eb583e37626e90d4
Raquel Marques <araquelfmarques@gmail.com>
2020-11-27 11:47:27 +0000

    ohohoh

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+
+$( window ).load(function() {
+  var x = document.getElementById("myAudio"); 
+  x.play();   
+});
+
+var x = document.getElementById("myAudio"); 
+
+function playAudio() { 
+  x.play(); 
+  $('.pause').css('display','block');
+  $('.play').css('display','none');  
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+  $('.play').css('display','block');
+  $('.pause').css('display','none');
+} 
+
+var y = document.getElementById("hohoho"); 
+
+function playHohoho() { 
+  y.play(); 
+   
+} 
+
 var click_numbers = 0;
 $('.square').on('click',function(){
     click_numbers ++;
@@ -11,12 +38,15 @@ $('.square').on('click',function(){
          $('#status-'+click_numbers).html('NOT OK');
          $('.gif').css('display','block');
          $('.start').css('display','block');
+         pauseAudio();
         
     }
     if (click_numbers === 3 && click_numbers == $(this).attr('order')){     
         click_numbers=0;
         $('.gif2').css('display','block');
         $('.card').css('display','block');
+        pauseAudio();
+        playHohoho();
     }
 });
 
